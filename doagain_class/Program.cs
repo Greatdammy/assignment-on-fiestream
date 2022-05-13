a3077d38f3495b85d2a13bd324c30a003117a3b2
DAMILARE OBAFEMI <damimmanuel7@gmail.com>
2022-05-13 15:56:17 +0100

    Add project files.

---
+++ doagain_class/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace doagain_class
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+            var data = filestream.GetFilestreams();
+
+
+          // filestream Write 
+
+            string path = @"C:\Users\H0me1\Videos\Data.txt";
+            FileStream fs = File.Open(path, FileMode.OpenOrCreate);
+            Encoding u8 = Encoding.UTF8;
+            
+            
+            {
+                byte[] bytlist = data.SelectMany(x => u8.GetBytes(x.)).ToArray();
+                fs.Write(bytlist, 0, bytlist.Length);
+                fs.Position = 0;
+                
+
+            }
+            System.Console.Read();
+
+
+
+            //filestream Read 
+
+            //byte[] filelist = new byte[bytlist.Length];
+            //for (int i = 0; i < bytlist.Length; i++)
+            //{
+            //    filelist[i] = (byte)fs.ReadByte();
+            //}
+            //Console.WriteLine(Encoding.Default.GetString(filelist));
+            //fs.Close();
+        }
+    }
+}
---
+++ doagain_class/filestream.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doagain_class
+{
+    public class filestream
+    {
+     
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Class  { get; set; }  
+        public decimal Grade { get; set; }
+
+
+        public static List<filestream> GetFilestreams()
+        {
+            return new List<filestream>
+            {
+                new filestream {Name = "Jumoke", Age = 26, Class = "Year 4", Grade = 4 },
+                new filestream {Name = "Moyosore", Age = 24, Class = "Year 3", Grade = 5 },
+                new filestream {Name = "Jimmy", Age = 24, Class = "Year 5", Grade = 7 },
+                new filestream {Name = "Damilare", Age = 22, Class = "Year 5", Grade = 6 },
+                new filestream {Name = "Ebuka", Age = 25, Class = "Year 4", Grade = 4 },
+                new filestream {Name = "Olabisi", Age = 21, Class = "Year 3", Grade = 6 },
+
+            };
+        }
+
+
+
+    }
+}
